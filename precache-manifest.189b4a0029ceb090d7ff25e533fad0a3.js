76794b330b6212cc94d10ef9cca32990eb09dbbb
Andres Cenci <andres.cenci@gmail.com>
2020-08-24 04:38:18 -0300

    Updates

---
+++ precache-manifest.189b4a0029ceb090d7ff25e533fad0a3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d18e7cfa9cc0e48649a5b9d539ad7ab",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "0b3f92edb94a5301f77e",
+    "url": "/pokedex/static/css/main.9548ed2d.chunk.css"
+  },
+  {
+    "revision": "cfc6aeeb38dec024160b",
+    "url": "/pokedex/static/js/2.5fda1097.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.5fda1097.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b3f92edb94a5301f77e",
+    "url": "/pokedex/static/js/main.effb2a5a.chunk.js"
+  },
+  {
+    "revision": "5d5b7317cf43a0b8925c",
+    "url": "/pokedex/static/js/runtime-main.2d94522c.js"
+  },
+  {
+    "revision": "fc0256c267123446c5622e966aef2f91",
+    "url": "/pokedex/static/media/arrow.fc0256c2.png"
+  },
+  {
+    "revision": "f228fdd55ec7477271dee51823570651",
+    "url": "/pokedex/static/media/not-available.f228fdd5.png"
+  },
+  {
+    "revision": "6bfedd95bf2a2fe9c25a1add56555191",
+    "url": "/pokedex/static/media/pokemon-logo.6bfedd95.png"
+  }
+]);
